ada70956718feafdb2ee0bcdc74db6f1e43c8a1f
okyk738 <60845352+okyk738@users.noreply.github.com>
2020-02-10 14:28:39 +0300

    Update base.js

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,4 +1,4 @@
-var baseURL = "https://akshatmittal.com/youtube-realtime/";
+var baseURL = "https://okyk738.github.io/ytsubstool/";
 if (typeof isCustomPage == 'undefined') isCustomPage = 0;
 if (window.location.hostname.indexOf("local.akshatmittal.com") < 0) {
     if (window.location.protocol != "https:") window.location.replace("https:" + window.location.href.substring(window.location.protocol.length));
